4050dbdbbfeba0abdce7e19321374df6516df0c3
Manmon-ammar <manmon807@gmail.com>
2025-02-27 15:48:46 +0200

    Booking form initial UI

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,10 @@
+import BookingForm from "@/components/bookingForm/BookingForm";
+
+export default function BookingPage() {
+    return <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <BookingForm/>
+      </div>
+    </>
+  }
+  
